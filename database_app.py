3d73499d851b2326bb471113ef03ac5259f121bd
macloo <mmcadams@gmail.com>
2016-04-16 17:26:03 -0400

    first commit

---
+++ database_app.py
@@ -0,0 +1,62 @@
+#!/Users/username/Documents/python/projectname/env/bin/python
+import pymysql
+from flask import Flask, render_template, session, redirect, url_for
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some secret string here'
+
+userpass = 'mysql+pymysql://root:@'
+basedir  = '127.0.0.1'
+dbname   = '/sockmarket'
+socket   = '?unix_socket=/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
+dbname   = dbname + socket
+app.config['SQLALCHEMY_DATABASE_URI'] = userpass + basedir + dbname
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+# each table in the database is a class
+class Sock(db.Model):
+    __tablename__ = 'socks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    style = db.Column(db.String)
+    color = db.Column(db.String)
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    updated = db.Column(db.String)
+
+    def __init__(self, name, style, color, quantity, price, updated):
+        self.name = name
+        self.style = style
+        self.color = color
+        self.quantity = quantity
+        self.price = price
+        self.updated = updated
+
+    def __repr__(self):
+        return '<Sock %s>' % self.name
+
+@app.route('/')
+def index():
+    socks = Sock.query.all()
+    foo = Sock.query.filter_by(id='16').first()
+    return render_template('index.html', socks=socks, foo=foo)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ socks.py
@@ -0,0 +1,57 @@
+import MySQLdb
+from flask import Flask, render_template, session, redirect, url_for
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some secret string here'
+
+userpass = 'mysql://macloo:paTGIL2277@'
+basedir  = 'macloo.mysql.pythonanywhere-services.com'
+dbname   = '/macloo$sockmarket'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = userpass + basedir + dbname
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+# each table in the database is a class
+class Sock(db.Model):
+    __tablename__ = 'socks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    style = db.Column(db.String)
+    color = db.Column(db.String)
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    updated = db.Column(db.String)
+
+    def __init__(self, name, style, color, quantity, price, updated):
+        self.name = name
+        self.style = style
+        self.color = color
+        self.quantity = quantity
+        self.price = price
+        self.updated = updated
+
+    def __repr__(self):
+        return '<Sock %s>' % self.name
+
+@app.route('/')
+def index():
+    socks = Sock.query.all()
+    foo = Sock.query.filter_by(id='16').first()
+    return render_template('index.html', socks=socks, foo=foo)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
